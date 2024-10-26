8975edb738df7941021a2ddf9a3d9b1153f3d960
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2024-10-26 10:42:26 -0400

    final enhance for today

--- resources/views/components/website/whyus.blade.php
+++ resources/views/components/website/whyus.blade.php
@@ -3,11 +3,10 @@
 
 <section class="py-16  text-primary bg-gray-100">
     <div class="container mx-auto px-4">
-        <h2 class="text-2xl font-semibold text-center mb-2">Our Features</h2>
+        <h2 class="text-2xl font-semibold text-center mb-2">Our Commitment to Excellence</h2>
         <h1 class="text-4xl font-bold text-center mb-6">Why Choose Us!</h1>
         <p class="text-center mb-12 max-w-3xl mx-auto">
-            Choose us for unparalleled quality, expertise, and innovation. With a customer-centric approach, we tailor flexible solutions to meet your unique needs while ensuring reliability and cost-effectiveness. Our proven track record, coupled with continuous support, establishes long-term partnerships aimed at your success. Join us on your journey to excellence.
-        </p>
+            Discover why we’re the preferred choice for AFPs. With a client-first approach, we deliver efficient, tailored solutions that drive your business toward achieving a Fantastic Plus rating. Our team combines expertise, transparency, and accountability to ensure your success, supported by consistent performance and comprehensive services.        </p>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div class="feature-card bg-primary p-6 rounded-lg transition-all duration-300 transform hover:-translate-y-2">
                 <div class="mb-4">
@@ -16,7 +15,7 @@
                     </svg>
                 </div>
                 <h3 class="text-xl font-semibold mb-2 text-white">Reliability</h3>
-                <p class="text-gray-300">We are committed to reliability, ensuring that your goods are transported safely and on time, every time.</p>
+                <p class="text-gray-300">We guarantee that your loads are managed efficiently, meeting Amazon's strict schedules and safety standards on every trip.</p>
             </div>
             <div class="feature-card bg-primary p-6 rounded-lg transition-all duration-300 transform hover:-translate-y-2">
                 <div class="mb-4">
@@ -24,8 +23,8 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </div>
-                <h3 class="text-xl font-semibold mb-2 text-white">Cost-Effectiveness</h3>
-                <p class="text-gray-300">Our streamlined dispatch processes help you with the minimize costs and maximize efficiency.</p>
+                <h3 class="text-xl font-semibold mb-2 text-white">Cost Efficiency</h3>
+                <p class="text-gray-300">Through optimized dispatching and proactive planning, we help you keep costs down while maintaining the highest level of service.</p>
             </div>
             <div class="feature-card bg-primary p-6 rounded-lg transition-all duration-300 transform hover:-translate-y-2">
                 <div class="mb-4">
@@ -35,7 +34,7 @@
                     </svg>
                 </div>
                 <h3 class="text-xl font-semibold mb-2 text-white">Customized Solutions</h3>
-                <p class="text-gray-300">We offer personalized dispatch solutions tailored to meet your specific needs and requirements.</p>
+                <p class="text-gray-300">We understand that every AFP has unique needs. Our flexible solutions are designed to your specific goals, ensuring alignment with Amazon's high expectations.</p>
             </div>
             <div class="feature-card bg-primary p-6 rounded-lg transition-all duration-300 transform hover:-translate-y-2">
                 <div class="mb-4">
@@ -43,8 +42,8 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
                 </div>
-                <h3 class="text-xl font-semibold mb-2 text-white">Transparency</h3>
-                <p class="text-gray-300">We value clear communication and transparency, keeping you updated throughout dispatch process.</p>
+                <h3 class="text-xl font-semibold mb-2 text-white">Customized Solutions and transparency</h3>
+                <p class="text-gray-300">We understand that every AFP has unique needs. Our flexible solutions are designed to your specific goals, ensuring alignment with Amazon's high expectations. We will keep you informed at every step, from dispatch updates to safety insights, so you're always in the loop.</p>
             </div>
         </div>
     </div>
