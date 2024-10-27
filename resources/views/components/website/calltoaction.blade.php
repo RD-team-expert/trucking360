042ee9713eadf1869eb9f0c17d6b689e90dc6618
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2024-10-27 10:49:25 -0400

    changes

--- resources/views/components/website/calltoaction.blade.php
+++ resources/views/components/website/calltoaction.blade.php
@@ -1,7 +1,7 @@
 
 
     <!-- Parallax Section -->
-    <section class="parallax py-20" style="background-image: url('{{asset('website/img/para.jpg')}}');">
+    <section class="parallax py-20" style="background-image: url('{{asset('website/img/paral.jpg')}}');">
       <div class="container mx-auto px-6 text-center">
           <div class="bg-white bg-opacity-80 p-8 rounded-lg shadow-lg inline-block">
               <h2 class="text-3xl font-bold text-primary mb-4">Ready to Optimize Your Logistics?</h2>
