3fbe2d8778714cd6ec091a75adf88012aa2ee294
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2025-01-15 12:36:21 -0500

    add meta tag to app blade

--- resources/views/layouts/website/app.blade.php
+++ resources/views/layouts/website/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
    
     {!! SEOTools::generate() !!}
     {{-- <link href="{{ asset('website/css/output.css') }}" rel="stylesheet">
